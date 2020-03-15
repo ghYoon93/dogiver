f04d9d9096336d46d7000fddcd17c75e65bac4df
Jinsup9k <01077717141p@gmail.com>
2020-03-15 17:31:36 +0900

    fix main

---
+++ dogner/WebContent/js/main.js
@@ -0,0 +1,20 @@
+var currentScrollTop = 0;
+
+window.onload = function() {
+  scrollController();
+
+  $(window).on("scroll", function() {
+    scrollController();
+  });
+};
+
+// 영역의 위치를 설정한 위치에서 부드럽게 고정시킴 - 시작
+function scrollController() {
+  currentScrollTop = $(window).scrollTop();
+
+  if ($(this).scrollTop() > 50) {
+    $("header").addClass("sticky");
+  } else {
+    $("header").removeClass("sticky");
+  }
+}
