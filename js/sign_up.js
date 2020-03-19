8dfb4e0f1fa4a06ba38dec1b8a0820d44e5a06ac
Jinsup9k <01077717141p@gmail.com>
2020-03-19 22:33:15 +0900

    modal 추가 및 css  변경

--- js/sign_up.js
+++ js/sign_up.js
@@ -1,14 +1,25 @@
-function email_confirm() {
-  var popupX = document.body.offsetWidth / 2 - 500 / 2;
-  var popupY = document.body.offsetHeight / 2 - 600 / 2;
+// Get the modal
+var modal = document.getElementById("myModal");
 
-  window.open(
-    "",
-    "이메일인증",
-    "status=no, height=600, width=500, left=" +
-      popupX +
-      ", top=" +
-      popupY +
-      ", location=0, toolbar=no, menubar=no"
-  );
-}
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+};
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+// window.onclick = function(event) {
+//   if (event.target == modal) {
+//     modal.style.display = "none";
+//   }
+// };
