d1e8588f15cd3a97cf5d9c1b387321053d799922
jwb0501 <jwb0501@gmail.com>
2020-03-23 17:34:13 +0900

    마스터병합

--- src/main/webapp/js/main.js
+++ src/main/webapp/js/main.js
@@ -2,7 +2,12 @@ var currentScrollTop = 0;
 
 window.onload = function() {
   scrollController();
+<<<<<<< HEAD
   loadTemplate();
+=======
+  $("#headerDiv").load("../template/header.html");
+  $("#footerDiv").load("../template/footer.html");
+>>>>>>> master
   $(window).on("scroll", function() {
     scrollController();
   });
@@ -18,6 +23,7 @@ function scrollController() {
     $("header").removeClass("sticky");
   }
 }
+<<<<<<< HEAD
 function loadTemplate(){
 	//인클루드
 	$('#header').load("../template/header.html");
@@ -25,3 +31,5 @@ function loadTemplate(){
 }
 
 
+=======
+>>>>>>> master
