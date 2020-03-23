319edd33b03ec1a5fb0ad7dd04c41346f9ea10b4
ghYoon93 <gh.yoon93@gmail.com>
2020-03-24 02:44:05 +0900

    orderDAO 생성

--- src/main/java/order/dao/OrderDAOMyBatis.java
+++ src/main/java/order/dao/OrderDAOMyBatis.java
@@ -1,5 +1,10 @@
 package order.dao;
 
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Repository
+@Transactional
 public class OrderDAOMyBatis implements OrderDAO {
 
 }
