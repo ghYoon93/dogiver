b28e445bdaa5ededd19a5427c24b38e418a8eab9
ghYoon93 <gh.yoon93@gmail.com>
2020-03-24 02:43:42 +0900

    OrderService 생성

--- src/main/java/order/service/OrderService.java
+++ src/main/java/order/service/OrderService.java
@@ -1,5 +1,11 @@
 package order.service;
 
+import java.util.List;
+
+import order.bean.CartDTO;
+
 public interface OrderService {
+
+	List<CartDTO> getCart();
 
 }
--- src/main/java/order/service/OrderServiceImpl.java
+++ src/main/java/order/service/OrderServiceImpl.java
@@ -1,5 +1,17 @@
 package order.service;
 
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import order.bean.CartDTO;
+@Service
 public class OrderServiceImpl implements OrderService {
+
+	@Override
+	public List<CartDTO> getCart() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
