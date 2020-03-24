545dfd0e18f64470a25e1ddc8472f94807168307
jefforle <jefforle20@gmail.com>
2020-03-25 02:37:07 +0900

    goodsDTO 자료형 수정

--- src/main/java/goods/bean/GoodsDTO.java
+++ src/main/java/goods/bean/GoodsDTO.java
@@ -7,11 +7,11 @@ import lombok.Data;
 
 @Data
 public class GoodsDTO {
-	private double goods_id;
-	private double category_code;
+	private int goods_id;
+	private int category_code;
 	private String goods_name;
 	private int goods_price;
-	private Date sysdate;
+	private Date goods_date;
 	private String goods_thumbnail;
 	private int goods_amt;
 	private String sales_yn;
