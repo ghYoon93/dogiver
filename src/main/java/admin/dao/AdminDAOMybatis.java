69fb0e22ed0a793f95caab35bb4dbd450d326294
jwb0501 <jwb0501@gmail.com>
2020-03-25 20:53:40 +0900

    관리자 페이지 수정

---
+++ src/main/java/admin/dao/AdminDAOMybatis.java
@@ -0,0 +1,21 @@
+package admin.dao;
+
+import java.util.Map;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+@Repository("AdminDAO")
+@Transactional
+public class AdminDAOMybatis implements AdminDAO {
+	@Autowired
+	private SqlSession sqlSession;
+
+	@Override
+	public Map<String, String> getDogiver() {
+		return null;
+	}
+
+}
