6ab83df6e6f3f6a935ab982eddeda653991952b2
ruddn615 <ruddn615@gmail.com>
2020-03-30 13:13:05 +0900

    no message

---
+++ src/main/webapp/js/reply.js
@@ -0,0 +1,21 @@
+$('#boardFormBtn').click(function() {
+	$('#contentDiv').empty();
+
+	if ($('#content').val() == '') {
+		$('#contentDiv').text('내용을 입력하세요');
+		$('#contentDiv').css('color', 'red')
+		$('#contentDiv').css('font-size', '8pt')
+		$('#contentDiv').css('font-weight', 'bold')
+	} else
+		var re_content = $('#re_content').val();
+		$.ajax({
+		type : 'POST',
+		url : '/dogiver/board/boardReply',
+		data : {'re_content' : re_content},
+		success : function(data) {
+			alert("댓글이 작성되었습니다.");
+			//작성되자마자 데이터가 올라가야한다 
+			//re_list();함수 호출해서 뿌려줘야함 .
+		}
+	});
+});
