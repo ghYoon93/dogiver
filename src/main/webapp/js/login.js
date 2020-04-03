617901ea9044dd2c312a4a96f4492ebfca127382
jwb0501 <jwb0501@gmail.com>
2020-04-03 11:59:08 +0900

    점검 수정

--- src/main/webapp/js/login.js
+++ src/main/webapp/js/login.js
@@ -113,7 +113,9 @@ $(document).ready(function() {
 				dataType: 'text',
 				success : function(data) {
 					if (data == "true") {
-				        location.reload();
+						let target = document.referrer;
+						if(target == "http://localhost:8080/dogiver/logout/logout") target="http://localhost:8080/dogiver/main/index";
+				        location.href = target;
 					} else {
 						$('.loginMessage').text('로그인 실패~ㅋ').show("slow").css("color","red");
 						setTimeout(function() {
