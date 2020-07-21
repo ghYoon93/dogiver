b2a4797e0f15724c278cbdd511892dabc35cc98a
ghYoon93 <gh.yoon93@gmail.com>
2020-07-22 01:54:36 +0900

    chore: profile 추가

--- src/main/resources/sql/db-test-data.sql
+++ src/main/resources/sql/db-test-data.sql
@@ -2,7 +2,7 @@ Insert into MEMBER (EMAIL,NAME,NICKNAME,PWD,PHONE,ZIPCODE,ADDR,ADDR_DETAIL,SEQ,R
 
 Insert into CATEGORY (CATEGORY_CODE,CATEGORY_NAME) values (101,'장난감');
 
-Insert into GOODS (GOODS_ID,CATEGORY_CODE,GOODS_NAME,GOODS_PRICE,GOODS_DATE,GOODS_THUMBNAIL,GOODS_AMT,SALES_YN,GOODS_DETAIL,GOODS_CUM_SALES) values (1010001,101,'핸드메이드 강아지 로프 장난감',3000,to_date('20/04/04','RR/MM/DD'),'1010001/thumbnail.jpg',1,'N','1010001/detail.jpg',71);
+Insert into GOODS (GOODS_ID,CATEGORY_CODE,GOODS_NAME,GOODS_PRICE,GOODS_DATE,GOODS_THUMBNAIL,GOODS_AMT,SALES_YN,GOODS_DETAIL,GOODS_CUM_SALES) values (1010001,101,'핸드메이드 강아지 로프 장난감',3000,to_date('20/04/04','RR/MM/DD'),'1010001/thumbnail.jpg',1,'Y','1010001/detail.jpg',71);
 
 INSERT INTO cart(cart_id, email, goods_id, cart_cnt) 
                 VALUES(seq_cart.nextval, 'gh.yoon93@gmail.com', 1010001, 3);
