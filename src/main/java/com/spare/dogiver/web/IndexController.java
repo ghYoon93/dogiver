0f135c6574f561907fc3f9a3173952f296484a37
ghYoon93 <gh.yoon93@gmail.com>
2020-11-30 02:06:49 +0900

    GET /cart 추가

--- src/main/java/com/spare/dogiver/web/IndexController.java
+++ src/main/java/com/spare/dogiver/web/IndexController.java
@@ -1,12 +1,21 @@
 package com.spare.dogiver.web;
 
+
+import java.util.List;
+
 import javax.servlet.http.HttpSession;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.spare.dogiver.service.cart.CartService;
+import com.spare.dogiver.service.cart.CartServiceImpl;
+import com.spare.dogiver.service.order.OrderService;
+import com.spare.dogiver.service.order.OrderServiceImpl;
+import com.spare.dogiver.web.dto.CartResponseDto;
+import com.spare.dogiver.web.dto.CheckoutRequestDto;
 
 import lombok.RequiredArgsConstructor;
 
@@ -14,14 +23,36 @@ import lombok.RequiredArgsConstructor;
 @Controller
 public class IndexController {
 	private final CartService cartService;
+	private final OrderService orderSerivce;
+	
     @GetMapping("/")
     public String index() {
     	return "index";
     }
+    
     @GetMapping("/cart")
     public String cart(Model model, HttpSession session) {
     	String email = (String)session.getAttribute("memEmail");
-    	model.addAttribute("cart", cartService.findAllByEmailDesc(email));
+    	model.addAttribute("cart", cartService.getCart(email));
     	return "/cart/cart";
+    }
+    
+    @GetMapping("/cart/checkout")
+    public String checkoutDriectCart() {
+    	return "/order/order";
+    }
+    
+    @GetMapping("/direct/checkout")
+    public String checkoutDirect(
+    		HttpSession session,
+    		Model model, 
+    		@RequestParam("goods_id") long goodsId, 
+    		@RequestParam("quantity") int quantity
+    		) {
+    	
+    	String email = (String)session.getAttribute("memEmail");
+    	CheckoutRequestDto request = new CheckoutRequestDto();
+    	model.addAttribute("checkout", orderSerivce.getCheckout(email, request));
+    	return "/order/order";
     }
 }
--- src/test/java/com/spare/dogiver/web/IndexControllerTest.java
+++ src/test/java/com/spare/dogiver/web/IndexControllerTest.java
@@ -1,35 +1,58 @@
 package com.spare.dogiver.web;
 
-import static org.junit.Assert.*;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.anyString;
+import static org.mockito.Mockito.verify;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.Before;
 import org.junit.After;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+import org.springframework.mock.web.MockHttpSession;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
-import org.springframework.web.context.WebApplicationContext;
 import com.spare.dogiver.config.RootConfig;
 import com.spare.dogiver.config.ServletConfig;
+import com.spare.dogiver.domain.CartItem;
+import com.spare.dogiver.domain.Goods;
+import com.spare.dogiver.service.cart.CartServiceImpl;
+import com.spare.dogiver.web.dto.CartResponseDto;
 
-@RunWith(SpringJUnit4ClassRunner.class)
+@RunWith(SpringRunner.class)
 @ContextConfiguration(classes = {RootConfig.class, ServletConfig.class})
 @WebAppConfiguration
 public class IndexControllerTest {
-	@Autowired
-	private WebApplicationContext context;
+	@InjectMocks
+	private IndexController indexController;
+	
+	@Mock
+	private CartServiceImpl cartService;
+	
+	protected MockHttpSession session;
 	
 	private MockMvc mockMvc;
+	
 	@Before
 	public void setUp() {
-		this.mockMvc = MockMvcBuilders.webAppContextSetup(context).build();
+		MockitoAnnotations.initMocks(this);
+		this.mockMvc = MockMvcBuilders.standaloneSetup(indexController).build();
+		session = new MockHttpSession();
+    	session.setAttribute("memEmail", "gh.yoon93@gmail.com");
+    	
 	}
 	
 	
@@ -38,9 +61,33 @@ public class IndexControllerTest {
 	}
 
 	@Test
-	public void test() throws Exception {
-		String url = "/direct/checkout/goodsId=1001&quantity=3";
-		mockMvc.perform(get(url)).andExpect(status().isOk());
+	public void cart() throws Exception {
+		// given
+    	String email = (String)session.getAttribute("memEmail");
+    	assertThat(email).isEqualTo("gh.yoon93@gmail.com");
+    	
+    	CartItem cartItem = CartItem.builder()
+    			.id(1L)
+    			.cartCnt(3)
+    			.build();
+    	
+    	Goods goods = Goods.builder().goodsId(1010001L).goodsPrice(2000).build();
+    	
+    	CartResponseDto dto = new CartResponseDto(cartItem, goods);
+    	
+    	List<CartResponseDto> cart = new ArrayList();
+    	cart.add(dto);
+    	
+    	Mockito.when(cartService.getCart(anyString())).thenReturn(cart);
+    	    
+    	mockMvc.perform(get("/cart")
+    			.session(session))    	
+    			.andExpect(status().isOk())
+    			.andExpect(view().name("/cart/cart"))
+    			.andExpect(model().attributeExists("cart"))
+    			.andExpect(model().attribute("cart", cart));
+    	
+    	verify(cartService).getCart(anyString());
 	}
 
 }
