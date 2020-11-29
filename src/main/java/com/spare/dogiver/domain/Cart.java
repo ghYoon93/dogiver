6068cd8d2ae4f1736c9b61b281a99880206375a3
ghYoon93 <gh.yoon93@gmail.com>
2020-11-30 02:05:12 +0900

    도메인 필드 수정

--- src/main/java/com/spare/dogiver/domain/Cart.java
+++ src/main/java/com/spare/dogiver/domain/Cart.java
@@ -10,29 +10,35 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 public class Cart {
 	
-	private long cartId;
-	private Member member;
-	private Goods goods;
+	private Long id;
+//	private Member member;
+//	private Goods goods;
+	private String email;
 	private int cartCnt;
 	private Date createdDate;
 	private Date modifedDate;
 	
 	@Builder
-	public Cart(Member member, Goods goods, int cartCnt) {
-		this.member = member;
-		this.goods = goods;
-		this.cartCnt = cartCnt;
+	public Cart(Long id, String email) {
+		this.id = id;
+		this.email = email;
 	}
+//	@Builder
+//	public Cart(Member member, Goods goods, int cartCnt) {
+//		this.member = member;
+//		this.goods = goods;
+//		this.cartCnt = cartCnt;
+//	}
 	
 	public void update(int cartCnt) {
 		this.cartCnt = cartCnt;
 	}
 	
-	public int getTotalPrice() {
-		return this.cartCnt * this.goods.getGoodsPrice(); 
-	}
+//	public int getTotalPrice() {
+//		return this.cartCnt * this.goods.getGoodsPrice(); 
+//	}
 	
 	public boolean hasCartId() {
-		return this.cartId != 0L ? true : false;
+		return this.id != 0L ? true : false;
 	}
 }
