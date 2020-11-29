b201d0e2ff644c74f45109e3c791028a115ac185
ghYoon93 <gh.yoon93@gmail.com>
2020-11-30 02:08:58 +0900

    getCart 로직 수정

--- src/main/java/com/spare/dogiver/service/cart/CartServiceImpl.java
+++ src/main/java/com/spare/dogiver/service/cart/CartServiceImpl.java
@@ -1,5 +1,6 @@
 package com.spare.dogiver.service.cart;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -7,10 +8,14 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.spare.dogiver.domain.Cart;
+import com.spare.dogiver.domain.CartItem;
+import com.spare.dogiver.domain.Goods;
 import com.spare.dogiver.domain.Member;
 import com.spare.dogiver.persistence.cart.CartDao;
+import com.spare.dogiver.persistence.cartItem.CartItemDao;
+import com.spare.dogiver.persistence.goods.GoodsDAO;
 import com.spare.dogiver.persistence.member.MemberDAO;
-import com.spare.dogiver.web.dto.CartListResponseDto;
+import com.spare.dogiver.web.dto.CartsResponseDto;
 import com.spare.dogiver.web.dto.CartResponseDto;
 import com.spare.dogiver.web.dto.CartSaveRequestDto;
 import com.spare.dogiver.web.dto.CartUpdateRequestDto;
@@ -25,62 +30,75 @@ import lombok.extern.log4j.Log4j;
 public class CartServiceImpl implements CartService {
 	
 	private final CartDao cartDao;
-	private final MemberDAO memberDao;
+	private final CartItemDao cartItemDao;
+	private final GoodsDAO goodsDao;
+//	private final MemberDAO memberDao;
 	
-	@Transactional
-	@Override
-	public Long save(String email, CartSaveRequestDto requestDto) {
-		Member member = memberDao.find(email);
-		Cart cart = cartDao.findByEmailAndGoodsId(requestDto.toEntity(member)); 
-		
-		if(cart.hasCartId()) {
-			return 0L;
-		}
-		
-		log.info("장바구니에 담는 물품: "+ requestDto);
-		return cartDao.save(cart);
-	}
-    
-	@Transactional
-	@Override
-	public List<Cart> findAllDesc() {
-		log.info("전체 물품 조회");
-		return cartDao.findAllDesc();
-	}
-    
-	@Transactional
-	@Override
-	public Long update(long cartId, CartUpdateRequestDto requestDto) {
-		Cart cart = cartDao.findById(cartId);
-		cart.update(requestDto.getCartCnt());
-		cartDao.update(cart);
-		return cartId;
-	}
-    
 	@Transactional(readOnly = true)
 	@Override
-	public List<CartListResponseDto> findAllByEmailDesc(String email) {
-		
-		return cartDao.findAllByEmailDesc(email).stream()
-				.map(CartListResponseDto::new)
-				.collect(Collectors.toList());
+	public List<CartResponseDto> getCart(String email) {
+		List<CartResponseDto> cart = new ArrayList();
+		// email에 해당하는 cartId 구해오기
+		Long cartId = cartDao.findByEmail(email).getCartId();
+		// cartId에 해당하는 cartItem들 구해오기
+		List<CartItem> cartItems = cartItemDao.findAllByCartId(cartId);
+		// cartItem의 goodsId에 해당하는 Goods 구해오기
+		for(CartItem cartItem : cartItems) {
+			Long goodsId = cartItem.getGoodsId();
+			Goods goods = goodsDao.findGoodsById(goodsId);
+			cart.add(new CartResponseDto(cartItem, goods));
+		}
+		return cart;
 	}
-
-	@Override
-	public CartResponseDto findById(Long cartId) {
-		Cart cart = cartDao.findById(cartId);
-		return new CartResponseDto(cart);
-	}
-
-	@Override
-	public void delete(long cartId) {
-		cartDao.delete(cartId);
-	}
-
-	@Override
-	public void deleteByIdIn(List<Long> cartIds) {
-		cartDao.deleteByIdIn(cartIds);
-		
-	}
+	
+//	@Transactional
+//	@Override
+//	public Long save(String email, CartSaveRequestDto requestDto) {
+//		Member member = memberDao.find(email);
+//		Cart cart = cartDao.findByEmailAndGoodsId(requestDto.toEntity(member)); 
+//		
+//		if(cart.hasCartId()) {
+//			return 0L;
+//		}
+//		
+//		log.info("장바구니에 담는 물품: "+ requestDto);
+//		return cartDao.save(cart);
+//	}
+//    
+//	@Transactional
+//	@Override
+//	public List<Cart> findAllDesc() {
+//		log.info("전체 물품 조회");
+//		return cartDao.findAllDesc();
+//	}
+//    
+//	@Transactional
+//	@Override
+//	public Long update(long cartId, CartUpdateRequestDto requestDto) {
+//		Cart cart = cartDao.findById(cartId);
+//		cart.update(requestDto.getCartCnt());
+//		cartDao.update(cart);
+//		return cartId;
+//	}
+//    
+	
+//
+//	@Override
+//	public CartResponseDto findById(Long cartId) {
+//		Cart cart = cartDao.findById(cartId);
+////		return new CartResponseDto(cart);
+//		return null;
+//	}
+//
+//	@Override
+//	public void delete(long cartId) {
+//		cartDao.delete(cartId);
+//	}
+//
+//	@Override
+//	public void deleteByIdIn(List<Long> cartIds) {
+//		cartDao.deleteByIdIn(cartIds);
+//		
+//	}
 
 }
--- src/test/java/com/spare/dogiver/service/CartServiceTest.java
+++ src/test/java/com/spare/dogiver/service/CartServiceTest.java
@@ -2,147 +2,194 @@ package com.spare.dogiver.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
 import org.springframework.test.context.ContextConfiguration;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
 
 import com.spare.dogiver.domain.Cart;
+import com.spare.dogiver.domain.CartItem;
 import com.spare.dogiver.domain.Goods;
-import com.spare.dogiver.domain.Member;
 import com.spare.dogiver.persistence.cart.CartDao;
+import com.spare.dogiver.persistence.cartItem.CartItemDao;
+import com.spare.dogiver.persistence.goods.GoodsDAO;
 import com.spare.dogiver.service.cart.CartService;
-import com.spare.dogiver.web.dto.CartListResponseDto;
+import com.spare.dogiver.service.cart.CartServiceImpl;
 import com.spare.dogiver.web.dto.CartResponseDto;
-import com.spare.dogiver.web.dto.CartSaveRequestDto;
-import com.spare.dogiver.web.dto.CartUpdateRequestDto;
+
+import static org.mockito.BDDMockito.given;
 
 
-@RunWith(SpringJUnit4ClassRunner.class)
+@RunWith(SpringRunner.class)
 @ContextConfiguration(classes = { com.spare.dogiver.config.RootConfig.class })
 public class CartServiceTest {
-    
-	@Autowired
 	private CartService cartService;
-	@Autowired
+	
+	@Mock
 	private CartDao cartDao;
 	
+	@Mock
+	private CartItemDao cartItemDao;
+	
+	@Mock
+	private GoodsDAO goodsDao;
+	
 	@Before
-	public void cleanUp() {
+	public void setUp() {
+		MockitoAnnotations.initMocks(this);
+		mockCartDao();
+		mockCartItemDao();
+		mockGoodsDao();
 		cartDao.deleteAll();
+		cartService = new CartServiceImpl(cartDao,cartItemDao, goodsDao);
 	}
 	
-	@Test
-	public void 장바구니_물품_담기() {
-		// given
+	private void mockCartDao() {
 		String email = "gh.yoon93@gmail.com";
-		Goods goods = Goods.builder().goodsId(1010001).build();
-		int cartCnt = 3;
-		CartSaveRequestDto requestDto = CartSaveRequestDto.builder()
-				.goods(goods)
-				.cartCnt(cartCnt).build();
-		
-		// when
-		long cartId = cartService.save(email, requestDto);
-		
-		
-		// then
-		Cart cart = cartDao.findById(cartId);
-		assertThat(cartCnt).isEqualTo(cart.getCartCnt());
+		Cart cart = Cart.builder().id(101L).email(email).build();
+		given(cartDao.findByEmail(email)).willReturn(cart);
+	}
+	
+	private void mockCartItemDao() {
+		List<CartItem> cartItems = new ArrayList();
+		CartItem cartItem = CartItem.builder().id(1001L).cartCnt(3).goodsId(1010001L).build();
+		cartItems.add(cartItem);
+		given(cartItemDao.findAllByCartId(101L)).willReturn(cartItems);
+	}
+	
+	private void mockGoodsDao() {
+		Goods goods = Goods.builder()
+				.goodsId(1010001L)
+				.goodsThumbnail("1010001/thumbnail.jpg")
+				.goodsName("핸드메이드 강아지 로프 장난감")
+				.goodsPrice(3000)
+				.build();
+		given(goodsDao.findGoodsById(1010001L)).willReturn(goods);
 	}
 	
 	@Test
-	public void 장바구니_물품_목록() {
-		// given
-		Member member = Member.builder().email("gh.yoon93@gmail.com").build();
-		Goods goods = Goods.builder().goodsId(1010001).build();
-		int cartCnt = 3;
+	public void getCart() {
+		List<CartResponseDto> cart = cartService.getCart("gh.yoon93@gmail.com");
+		CartResponseDto dto = cart.get(0);
+		assertThat(dto.getCartCnt()).isEqualTo(3);
+		assertThat(dto.getGoodsName()).isEqualTo("핸드메이드 강아지 로프 장난감");
 		
-		Cart cart = Cart.builder()
-				.member(member)
-				.goods(goods)
-				.cartCnt(cartCnt).build();
 		
-		cartDao.save(cart);
-		
-		// when
-		List<CartListResponseDto> carts = cartService.findAllByEmailDesc("gh.yoon93@gmail.com");
-		
-		// then
-		List<Cart> all = cartDao.findAll();
-		assertThat(all.get(0).getGoods().getGoodsId()).isEqualTo(carts.get(0).getGoods().getGoodsId());
 	}
 	
-	@Test
-	public void 장바구니_물품_조회() {
-		// given
-		Member member = Member.builder().email("gh.yoon93@gmail.com").build();
-		Goods goods = Goods.builder().goodsId(1010001).build();
-		int cartCnt = 3;
-		
-		Cart cart = Cart.builder()
-				.member(member)
-				.goods(goods)
-				.cartCnt(cartCnt).build();
-		
-		long exPectedCartId = cartDao.save(cart);
-		
-		// when
-		CartResponseDto expectedCart = cartService.findById(exPectedCartId);
-		
-		// then
-		assertThat(expectedCart.getCartCnt()).isEqualTo(cartCnt);
-	}
-	
-	@Test
-	public void 장바구니_물품_수량_변경() {
-		// given
-		Member member = Member.builder().email("gh.yoon93@gmail.com").build();
-		Goods goods = Goods.builder().goodsId(1010001).build();
-		int cartCnt = 3;
-		
-		Cart cart = Cart.builder()
-				.member(member)
-				.goods(goods)
-				.cartCnt(cartCnt).build();
-		
-		long savedCartId = cartDao.save(cart);
-		
-		int expectedCartCnt = 5;
-		
-		CartUpdateRequestDto requestDto = CartUpdateRequestDto.builder()
-				.cartCnt(expectedCartCnt).build();
-		// when
-		cartService.update(savedCartId, requestDto);
-		
-		// then
-		List<Cart> carts = cartDao.findAll();
-		assertThat(carts.get(0).getCartCnt()).isEqualTo(expectedCartCnt);
-	}
-	@Test
-	public void 장바구니_물품_삭제() {
-		// given
-		Member member = Member.builder().email("gh.yoon93@gmail.com").build();
-		Goods goods = Goods.builder().goodsId(1010001).build();
-		int cartCnt = 3;
-		
-		Cart cart = Cart.builder()
-				.member(member)
-				.goods(goods)
-				.cartCnt(cartCnt).build();
-		
-		long cartId = cartDao.save(cart);
-		
-		// when
-		cartService.delete(cartId);
-		
-		// then
-		List<Cart> all = cartDao.findAll();
-		assertThat(all).isEmpty();
-		
-	}
+//	@Test
+//	public void 장바구니_물품_담기() {
+//		// given
+//		String email = "gh.yoon93@gmail.com";
+//		Goods goods = Goods.builder().goodsId(1010001).build();
+//		int cartCnt = 3;
+//		CartSaveRequestDto requestDto = CartSaveRequestDto.builder()
+//				.goods(goods)
+//				.cartCnt(cartCnt).build();
+//		
+//		// when
+//		long cartId = cartService.save(email, requestDto);
+//		
+//		
+//		// then
+//		Cart cart = cartDao.findById(cartId);
+//		assertThat(cartCnt).isEqualTo(cart.getCartCnt());
+//	}
+//	
+//	@Test
+//	public void 장바구니_물품_목록() {
+//		// given
+//		Member member = Member.builder().email("gh.yoon93@gmail.com").build();
+//		Goods goods = Goods.builder().goodsId(1010001).build();
+//		int cartCnt = 3;
+//		
+//		Cart cart = Cart.builder()
+//				.member(member)
+//				.goods(goods)
+//				.cartCnt(cartCnt).build();
+//		
+//		cartDao.save(cart);
+//		
+//		// when
+//		List<CartsResponseDto> carts = cartService.getCart("gh.yoon93@gmail.com");
+//		
+//		// then
+//		List<Cart> all = cartDao.findAll();
+//		assertThat(all.get(0).getGoods().getGoodsId()).isEqualTo(carts.get(0).getGoods().getGoodsId());
+//	}
+//	
+//	@Test
+//	public void 장바구니_물품_조회() {
+//		// given
+//		Member member = Member.builder().email("gh.yoon93@gmail.com").build();
+//		Goods goods = Goods.builder().goodsId(1010001).build();
+//		int cartCnt = 3;
+//		
+//		Cart cart = Cart.builder()
+//				.member(member)
+//				.goods(goods)
+//				.cartCnt(cartCnt).build();
+//		
+//		long exPectedCartId = cartDao.save(cart);
+//		
+//		// when
+//		CartResponseDto expectedCart = cartService.findById(exPectedCartId);
+//		
+//		// then
+//		assertThat(expectedCart.getCartCnt()).isEqualTo(cartCnt);
+//	}
+//	
+//	@Test
+//	public void 장바구니_물품_수량_변경() {
+//		// given
+//		Member member = Member.builder().email("gh.yoon93@gmail.com").build();
+//		Goods goods = Goods.builder().goodsId(1010001).build();
+//		int cartCnt = 3;
+//		
+//		Cart cart = Cart.builder()
+//				.member(member)
+//				.goods(goods)
+//				.cartCnt(cartCnt).build();
+//		
+//		long savedCartId = cartDao.save(cart);
+//		
+//		int expectedCartCnt = 5;
+//		
+//		CartUpdateRequestDto requestDto = CartUpdateRequestDto.builder()
+//				.cartCnt(expectedCartCnt).build();
+//		// when
+//		cartService.update(savedCartId, requestDto);
+//		
+//		// then
+//		List<Cart> carts = cartDao.findAll();
+//		assertThat(carts.get(0).getCartCnt()).isEqualTo(expectedCartCnt);
+//	}
+//	@Test
+//	public void 장바구니_물품_삭제() {
+//		// given
+//		Member member = Member.builder().email("gh.yoon93@gmail.com").build();
+//		Goods goods = Goods.builder().goodsId(1010001).build();
+//		int cartCnt = 3;
+//		
+//		Cart cart = Cart.builder()
+//				.member(member)
+//				.goods(goods)
+//				.cartCnt(cartCnt).build();
+//		
+//		long cartId = cartDao.save(cart);
+//		
+//		// when
+//		cartService.delete(cartId);
+//		
+//		// then
+//		List<Cart> all = cartDao.findAll();
+//		assertThat(all).isEmpty();
+//		
+//	}
 }
