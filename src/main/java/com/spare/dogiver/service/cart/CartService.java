e5d04623ffd3d84a922e5d62e4f17f4dc85848ca
ghYoon93 <gh.yoon93@gmail.com>
2020-11-30 02:11:43 +0900

    응답 Dto 변경

--- src/main/java/com/spare/dogiver/service/cart/CartService.java
+++ src/main/java/com/spare/dogiver/service/cart/CartService.java
@@ -3,17 +3,16 @@ package com.spare.dogiver.service.cart;
 import java.util.List;
 
 import com.spare.dogiver.domain.Cart;
-import com.spare.dogiver.web.dto.CartListResponseDto;
 import com.spare.dogiver.web.dto.CartResponseDto;
 import com.spare.dogiver.web.dto.CartSaveRequestDto;
 import com.spare.dogiver.web.dto.CartUpdateRequestDto;
 
 public interface CartService {
-	public Long save(String email, CartSaveRequestDto requestDto);
-	public List<Cart> findAllDesc();
-	public List<CartListResponseDto> findAllByEmailDesc(String email);
-	public CartResponseDto findById(Long cartId);
-	public Long update(long cartId, CartUpdateRequestDto requestDto);
-	public void delete(long cartId);
-	public void deleteByIdIn(List<Long> cartIds);
+	public List<CartResponseDto> getCart(String email);
+//	public Long save(String email, CartSaveRequestDto requestDto);
+//	public List<Cart> findAllDesc();
+//	public CartResponseDto findById(Long cartId);
+//	public Long update(long cartId, CartUpdateRequestDto requestDto);
+//	public void delete(long cartId);
+//	public void deleteByIdIn(List<Long> cartIds);
 }
